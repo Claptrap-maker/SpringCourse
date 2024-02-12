9ca575f6ad557960ee1ea9d49212ced75feacf74
Claptrap-maker <lelik575435@gmail.com>
2024-02-13 00:34:01 +0300

    Dependency Injection (внедрение примитивных значений вручную в applicationContext.xml)

--- src/main/java/ru/alishev/springcourse/MusicPlayer.java
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -3,6 +3,26 @@ package ru.alishev.springcourse;
 public class MusicPlayer {
     private Music music;
 
+    private String name;
+
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
     // Inversion of Control (внедрение зависимости)
     public MusicPlayer(Music music) {
         this.music = music;
